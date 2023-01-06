134dbe4b10ad9ec4a7c91d982708a890927822fc
Devops1 <devops1@gmail.com>
2023-01-07 01:17:50 +0530

    main file added

---
+++ terraform_lambda_api_gateway/main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  shared_credentials_file = "C:/Users/aman1/.aws/credentials"
+  region = var.aws_region
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
+data "archive_file" "lambda_spring_boot_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/spring-boot-hello-world"
+  output_path = "${path.module}/spring-boot-hello-world.zip"
+}
+
+resource "aws_s3_object" "lambda_spring_boot_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "spring-boot-hello-world.zip"
+  source = data.archive_file.lambda_spring_boot_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_spring_boot_hello_world.output_path)
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_spring_boot_hello_world.key
+
+  runtime = "${var.lambda_runtime}"
+  handler = "${var.lambda_function_handler}"
+
+  source_code_hash = data.archive_file.lambda_spring_boot_hello_world.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
